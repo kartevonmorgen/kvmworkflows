f9463ceb8919d31635e4b7b44067cfdf1e3c47da
Navid Kalaei <navidkalaei@gmail.com>
2025-03-19 10:00:31 +0000

    feat: add activity insert tags

--- kvmworkflows/activities/insert_tags.py
+++ kvmworkflows/activities/insert_tags.py
@@ -1,6 +1,6 @@
 from typing import Iterable
 from temporalio import activity
-from kvmworkflows.graphql.client import client
+from kvmworkflows.graphql.client import graphql_client
 from kvmworkflows.graphql.graphql_client.input_types import tags_insert_input
 
 
@@ -8,4 +8,4 @@ from kvmworkflows.graphql.graphql_client.input_types import tags_insert_input
 async def insert_tags(tags: Iterable[str]) -> None:
     inputs = list(map(lambda tag: tags_insert_input(id=tag), tags))
     
-    await client.insert_tags(inputs)
+    await graphql_client.insert_tags(inputs)
