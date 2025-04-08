af300505ea94f7126541c0919797a50b3583b1a4
Navid Kalaei <navidkalaei@gmail.com>
2025-04-08 11:26:21 +0000

    fix: update the installation of hasura

--- scripts/hasura/install_cli.sh
+++ scripts/hasura/install_cli.sh
@@ -5,3 +5,6 @@ curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor -o
 echo "deb [signed-by=/usr/share/keyrings/postgresql-keyring.gpg] http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 apt-get update
 apt-get install -y postgresql-client-17
+
+# Download and install Hasura CLI
+curl -L https://github.com/hasura/graphql-engine/raw/stable/cli/get.sh | bash
